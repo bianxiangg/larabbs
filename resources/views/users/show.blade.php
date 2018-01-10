88ab44708d907c99eda1c9a64218f4164bd87f99
vagrant <vagrant@homestead>
2018-01-10 03:19:13 +0000

    User can upload avatar

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -7,7 +7,7 @@
             <div class="panel-body">
                 <div class="media">
                     <div align="center">
-                        <img width="300px" height="300px" src="https://fsdhubcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/600/h/600" alt="" class="thumbnail img-responsive">
+                        <img width="300px" height="300px" src="{{ $user->avatar }}" alt="" class="thumbnail img-responsive">
                     </div>
                     <div class="media-body">
                         <hr>
