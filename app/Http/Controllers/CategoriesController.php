3cca0c8d0c0d7af2e178966e4d51a3e8664873bc
vagrant <vagrant@homestead>
2018-01-11 08:02:07 +0000

    List topics created by user

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -12,6 +12,7 @@ class CategoriesController extends Controller
         $topics = $topic->withOrder($request->order)
                         ->where('category_id', $category->id)
                         ->paginate(20);
+        // $topics = Topic::where('category_id', $category->id)->paginate(20);
         return view('topics.index', compact('topics', 'category'));
     }
 }
